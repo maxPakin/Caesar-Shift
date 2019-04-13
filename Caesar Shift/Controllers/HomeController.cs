fd8e0d54991e307b0d201be0dc0cbbe313f13500
Макс <pakinmaksim@mail.ru>
2019-04-14 02:19:17 +0300

    Just move in currect directory

--- Caesar Shift/Controllers/HomeController.cs
+++ Caesar Shift/Controllers/HomeController.cs
@@ -1,16 +1,31 @@
-﻿using System;
+﻿using Caesar_Shift.Business;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Xceed.Words.NET;
 
 namespace Caesar_Shift.Controllers
 {
     public class HomeController : Controller
     {
+        FileContext db = new FileContext();
         public ActionResult Index()
         {
+            db.Files.
             return View();
+        }
+
+        [HttpPost]
+        public string Upload(HttpPostedFileBase upload, string text, string action)
+        {
+            if (upload == null)
+            {
+                if ()
+            }
+
+            return action;
         }
 
         public ActionResult About()
