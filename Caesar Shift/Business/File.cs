06b9dd6c58c2dac765992911993a940b9347158d
Макс <pakinmaksim@mail.ru>
2019-04-13 16:06:35 +0300

    Creation File DataBase

---
+++ Caesar Shift/Business/File.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Caesar_Shift.Business
+{
+    public class File
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
---
+++ Caesar Shift/Business/FileContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Caesar_Shift.Business
+{
+    public class FileContext : DbContext
+    {
+        public DbSet<File> Files { get; set; }
+
+        
+
+    }
+}
